e0e4bb39a875535e3833b5a4c1b9caeb4fa47d8e
Alejandro Martínez <amrkratos@hotmail.com>
2020-07-02 09:11:54 -0500

    agregue mas usuarios

--- TwitterDB.sql
+++ TwitterDB.sql
@@ -12,5 +12,11 @@ CREATE TABLE usuario(
 );
 
 insert into usuario values('amrkratos@hotmail.com', 'Alejandro Martinez', null, 'hola1', 0, 0);
+insert into usuario values('juanito@hotmail.com', 'Juan Alvarez', null, 'hola12', 0, 0);
+insert into usuario values('lopez@hotmail.com', 'Lopez Eliot', null, 'hola13', 0, 0);
+insert into usuario values('pedro@hotmail.com', 'Pedro Emilio', null, 'hola14', 0, 0);
+insert into usuario values('andrea@hotmail.com', 'Andrea Morones', null, 'hola15', 0, 0);
+
+
 
 SELECT * FROM USUARIOS;
